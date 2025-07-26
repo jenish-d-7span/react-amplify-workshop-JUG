d1250630340f754f3d8aaba84dcf536b0afbbeb4
Jenish Dadhania <159224304+jenish-d-7span@users.noreply.github.com>
2025-07-26 12:22:02 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 // export const endpoint = 'http://15.206.164.156:8080';
-export const endpoint = 'https://743gyaarbc.execute-api.ap-south-1.amazonaws.com';
+export const endpoint = 'https://gqasnu6dj8.execute-api.ap-south-1.amazonaws.com';
 
 export const API_PATH = '/api/users';
